0b45b11a5412d179b8a7fc8e1c490787200e507f
Vellio <cvellio@gmail.com>
2022-08-04 13:10:39 +0300

    Добавление ассинхронного чтения файлов

--- server.js
+++ server.js
@@ -1,24 +1,65 @@
 const http = require("http");
+const fs = require("fs");
+
+// const app = express();
+// const express = require("express");
+// const favicon = require("serve-favicon");
+// const path = require("path");
+// app.use(express.static(path.join(__dirname, "public", "favicon.ico")));
+// app.get("/", (req, res) => {
+//   res.send("Hello");
+// });
+// app.listen(3003);
+
+const readFile = (path) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (err, data) => {
+      if (err) reject(err);
+      else resolve(data);
+    });
+  });
+};
+
+const delay = (ms) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+};
 
 let requestsCount = 0;
-const server = http.createServer((req, res) => {
-  if (req.url !== "/favicon.ico") requestsCount++;
+const server = http.createServer(async (req, res) => {
+  // if (req.url !== "/favicon.ico") {
+  //   requestsCount++;
+  //   res.write(` - Response counter: ${requestsCount}`);
+  // }
 
   switch (req.url) {
-    case "/students":
-      res.write("Students");
+    case "/home": {
+      try {
+        const data = await readFile("pages/home.html");
+        res.write(data);
+      } catch (err) {
+        res.write("Some error");
+      } finally {
+        res.end();
+      }
       break;
+    }
 
-    case "/":
-    case "/courses":
-      res.write("Courses");
+    case "/about": {
+      await delay(3000);
+      res.write("About");
+      res.end();
       break;
-    default:
+    }
+
+    default: {
       res.write("Not found");
+      res.end();
+    }
   }
-
-  res.write(` - Response counter: ${requestsCount}`);
-  res.end();
 });
 
 server.listen(3003);
